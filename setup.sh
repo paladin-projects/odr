1bfd66aff7db4a973285b82984e818462e7d69a2
Ivan Lyanguzov <ilyanguzov@paladin-group.ru>
2025-09-22 12:40:54 +0300

    Put configuration and script into user's appropriate dirs

--- setup.sh
+++ setup.sh
@@ -16,6 +16,17 @@ for i in ${PREREQ[*]}; do
     fi
 done
 
+echo "Checking for configuration file..."
+if [ ! -d "$HOME/.config/odr"]; then
+        mkdir -p $HOME/.config/odr
+fi
+if [ -f "$HOME/.config/odr/odr.conf"]; then
+        echo "Configuration file exists. Please, check ~/.config/odr/odr.conf contents and apply changes as needed"
+else
+        cp ./odr.conf $HOME/.config/odr
+        echo "Default configuration file copied to ~/.config/odr/odr.conf. Please, check its contents and apply changes as needed"
+fi
+
 echo "Checking SSH public key..."
 if [ -f "$HOME/.ssh/odr.pub" ]; then
     echo "SSH keys was found"
@@ -26,7 +37,7 @@ fi
 
 echo "Checking SSH config..."
 if [ -f "$HOME/.ssh/config" ]; then
-    echo "SSH config was found"
+    echo "SSH config was found. Please, check ~/.ssh/config file and apply changes as needed"
 else
     echo "Generating SSH config..."
     echo "Host *" > ~/.ssh/config
@@ -36,9 +47,35 @@ else
     echo "    IdentityFile ~/.ssh/odr" >> ~/.ssh/config
 fi
 
-echo "Check target array for SSH key registration with showsshkey command."
-echo "If the key is not registered, please register it manually with setsshkey command."
+echo "Check target 3PAR/Primera/Alletra9K/AlletraMP array for SSH public key registration with 'showsshkey' command."
+echo "If the key is not registered, please register it manually with 'setsshkey' command."
 echo "Use contents of ~/.ssh/odr.pub file."
+
+echo "For Brocade switch check that SSH public key is imported with 'sshutil showpubkeys' command."
+echo "If the key is not registered, please, import the key with 'sshutil importpubkey' command."
+echo "Use contents of ~/.ssh/odr.pub file."
+
+echo "Checking bin directory..."
+if [ ! -d ~/.local/bin ]; then
+	echo "Creating ~/.local/bin"
+	mkdir -p ~/.local/bin
+else
+	echo "Using ~/.local/bin"
+fi
+
+echo "Checking scripts..."
+for i in `ls -1 ./bin` ; do
+	ii=`which $i`
+	ret=$?
+	if [ $ret -eq 0 -a -n $ii ]; then
+		cmp -s $i $ii
+		ret=$?
+	fi
+	if [ $ret -eq 1 ]; then
+		echo "Copying $i to ~/.local/bin"
+		cp $i ~/.local/bin
+	fi
+done
 
 echo "Checking existing crontab entries..."
 CRONTAB=`crontab -l 2>/dev/null`
@@ -57,17 +94,15 @@ if [ -f "/etc/apache2/apache2.conf" ]; then
     echo "Configuration file for Apache web server prepared: odr-http.conf."
     echo "Edit it as needed."
     echo "Put it into /etc/apache2/sites-available/odr-http.conf and enable it with sudo a2ensite odr-http.conf"
-    echo "Then restart Apache web server with systemctl restart apache2."
-elif [ -f "/etc/nginx/nginx.conf" ]; then
-    echo "Nginx web server found"
+    echo "Then restart Apache web server with 'systemctl restart apache2'."
+elif [ -f "/etc/nginx/nginx.conf" -o -f "/etc/angie/angie.conf"]; then
+    echo "Nginx/Angie web server found"
     sed -e "s|%%HOME%%|$HOME|" nginx-odr-http.conf > odr-http.conf
-    echo "Configuration file for Nginx web server prepared: odr-http.conf."
+    echo "Configuration file for Nginx/Angie web server prepared: odr-http.conf."
     echo "Edit it as needed."
-    echo "Put it into /etc/nginx/sites-available/odr-http.conf and enable it with sudo ln -s /etc/nginx/sites-available/odr-http.conf /etc/nginx/sites-enabled/"
-    echo "Then restart Nginx web server with systemctl restart nginx."
-elif [ -f "/etc/angie/angie.conf" ]; then
-    echo "Angie web server found"
-    # Prepare Angie configs
+    echo "Put it into /etc/nginx/sites-available/odr-http.conf (for Nginx) or /etc/angie/sites-available/odr-http.conf (for Angie)"
+	echo "Enable site with 'ln -s /etc/nginx/sites-available/odr-http.conf /etc/nginx/sites-enabled/' (Nginx) or 'ln -s /etc/angie/sites-available/odr-http.conf /etc/angie/sites-enabled/'"
+    echo "Then restart Nginx/Angie web server with 'systemctl restart nginx' or 'systemctl restart angie'."
 else
-    echo "No known web servers found"
+    echo "No known web servers found. Prepare site configuration manually"
 fi
